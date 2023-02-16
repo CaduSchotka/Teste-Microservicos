8559d4c83c5b8d3350f24f72183efc47fdbf9fba
Carlos Schotka <111001953+CaduSchotka@users.noreply.github.com>
2023-02-16 13:16:56 -0300

    Add files via upload

---
+++ financeiro-php.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -d vendor ]; 
+then
+  composer install
+fi
+
+php index.php
---
+++ front-end.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -d node_modules ]; 
+then
+  npm install
+fi
+
+npm start -- --host=0.0.0.0 --port 4200
